836b1e303e14bf17a1c7f7c68bfb258a61900997
Sonantony <106673895+Sonantony@users.noreply.github.com>
2022-06-01 16:40:46 +0400

    Add files via upload
    1

---
+++ prog 2/hunter.js
@@ -0,0 +1,106 @@
+class Hunter {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.energy = 8
+        this.directions = [];
+    }
+    getNewCoordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+    chooseCell(character) {
+        this.getNewCoordinates()
+        var found = [];
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == character) {
+                    found.push(this.directions[i]);
+                }
+            }
+        }
+
+        return found;
+    }
+
+    mul() {
+        var emptyCells = this.chooseCell(0);
+        var newCell = random(emptyCells);
+
+        if (newCell) {
+            var newX = newCell[0];
+            var newY = newCell[1];
+            matrix[newY][newX] = 5;
+
+            var newGrassEater = new GrassEater(newX, newY);
+            grassEaterArr.push(newGrassEater);
+            this.energy = 30
+        }
+    }
+
+    move() {
+        this.energy--
+        var emptyCells = this.chooseCell(0);
+        var newCell = random(emptyCells);
+        if(newCell && this.energy >= 0) {
+            var newX = newCell[0];
+            var newY = newCell[1];
+            matrix[newY][newX] = matrix[this.y][this.x] ///kam 5 tiv@
+            matrix[this.y][this.x] = 0
+            this.x = newX
+            this.y = newY
+        } else {
+            this.die()
+        }
+    }
+
+    eat() {
+        var emptyCells = this.chooseCell(2,3);
+        var newCell = random(emptyCells);
+        if(newCell) {
+            this.energy+=10
+            var newX = newCell[0];
+            var newY = newCell[1];
+            matrix[newY][newX] = matrix[this.y][this.x] ///kam 5 tiv@
+            matrix[this.y][this.x] = 0
+            this.x = newX
+            this.y = newY
+            for (var i in grassEaterArr) {
+                if (newX == grassEaterArr[i].x && newY == grassEaterArr[i].y) {
+                    grassEaterArr.splice(i, 1);
+                    break;
+                }
+                for (var i in predatorArr) {
+                    if (newX == predatorArr[i].x && newY == predatorArr[i].y) {
+                        predatorArr.splice(i, 1);
+                        break;
+                    }
+            } }
+            
+            if(this.energy >= 12) {
+                this.mul()
+            }
+        } else {
+            this.move()
+        }
+    }
+    die() {
+        matrix[this.y][this.x] = 0
+        for (var i in hunterArr) {
+            if (this.x == hunterArr[i].x && this.y == hunterArr[i].y) {
+                hunterArr.splice(i, 1);
+                break;
+            }
+        }
+    }
+}
